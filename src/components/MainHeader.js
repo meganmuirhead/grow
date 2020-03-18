53dd7d8208fb4c2829c8c424020b5f6a38cfac73
Megan Muirhead <megan@s56.net>
2020-03-18 14:49:54 -0600

    working on adding card

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -11,31 +11,33 @@ class MainHeader extends Component {
         }
     }
 
+    stocks = [];
 
-
-    fetchStocks = (e) => {
-        console.log(1, e)
-            this.setState({search: e.target.value.substr(0, 20)})
-            if (!this.state.search) {
-                return;
-            }
+    storeInput = (e) => {
+        e.preventDefault();
+        this.setState({search: e.target.value.substr(0, 20)})
+        if (!this.state.search) {
+            return;
+        }
         console.log(2, this.state.search)
+
+    }
+    fetchStocks = (e) => {
+        e.preventDefault();
         fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${this.state.search}&apikey=HY0JP87WH3PG17X6`)
             .then(res => res.json())
             .then(data => {
                 this.setState({setSearch: data});
-                console.log(this.state.setSearch)
+                console.log(2, data)
+
+                this.stocks.push(data);
+
+
             })
     }
 
     render() {
-        //get rid of filtered stock
-        // let filteredStock  = this.props.stocks.filter(
-        //     (stock) =>  {
-        //         return stock.name.indexOf(this.state.
-        //             search) !== -1;
-        //     }
-        // )
+       
         return (
             <React.Fragment>
                 <nav className='header'>
@@ -50,18 +52,16 @@ class MainHeader extends Component {
                 </nav>
                 <div className="stock-text-box">
                     <h1>Stock Watcher</h1>
-                    <input type="text"  placeholder="Enter Stock Symbol" />
+                    <input type="text"  placeholder="Enter Stock Symbol" onChange={this.storeInput.bind(this)}/>
                     <button className="btn btn-full" onClick={this.fetchStocks.bind(this)}>Add Stock</button>
                     <button className="btn btn-ghost" >Delete</button>
                 </div>
                 <div className='container'>
-                    {/*do map off props.stock not hard coded filter*/}
 
 
-                    {/*{filteredStock.map(stock => {*/}
-                        {/*return <StockCard stock={stock}*/}
-                        {/*key={stock.id}/>*/}
-                    {/*})}*/}
+                    {this.stocks.map(stock => {
+                        return <StockCard stock={stock}/>
+                    })}
                 </div>
 
             </React.Fragment>
