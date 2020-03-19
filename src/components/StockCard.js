3f62f055f78a994e0041bfa8479c99367737543b
Megan Muirhead <megan@s56.net>
2020-03-18 21:21:03 -0600

    wip

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -7,22 +7,22 @@ const StockCard = (props) => {
     return(
         <div className="stock-card-parent"  onClick={props.onClickDelete}>
             <div className="percent">
-                <div> High: {props.stock['03. high']}</div>
-                <div> Low: {props.stock['04. low']}</div>
+                <div className="padding"> High: {props.stock['03. high']}</div>
+                <div className="padding"> Low: {props.stock['04. low']}</div>
             </div>
 
             <div className="stock-card">
 
                 <div className="card-flex"> {props.stock['01. symbol']}</div>
                 <div className="card-flex">
-                    <div> Price: {props.stock['05. price']}</div>
-                    <div> Change:{props.stock['09. change']}</div>
-                    <div> Percent Change:{props.stock['10. change percent']}</div>
+                    <div className="padding"> Price: {props.stock['05. price']}</div>
+                    <div className="padding"> Change:{props.stock['09. change']}</div>
+                    <div className="padding"> Percent Change:{props.stock['10. change percent']}</div>
                 </div>
-                <div className="card-flex">
-                    <div> Open: {props.stock['02. open']}</div>
-                    <div> High: {props.stock['03. high']}</div>
-                    <div> Low: {props.stock['04. low']}</div>
+                <div className="card-flex ">
+                    <div className="padding"> Open: {props.stock['02. open']}</div>
+                    <div className="padding"> High: {props.stock['03. high']}</div>
+                    <div className="padding"> Low: {props.stock['04. low']}</div>
                 </div>
             </div>
         </div>
