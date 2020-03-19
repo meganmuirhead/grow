dc50a7324e3b202d66406c5423772a58e1c9b02b
Megan Muirhead <megan@s56.net>
2020-03-18 21:09:14 -0600

    wip

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -5,19 +5,19 @@ const StockCard = (props) => {
     console.log('stock', props.stock)
 
     return(
-        <div className="stock-card-parent"  onClick={props.onClickDelete}>>
+        <div className="stock-card-parent"  onClick={props.onClickDelete}>
             <div className="percent">
-                <div> {props.stock['01. symbol']}</div>
-
+                <div> High: {props.stock['03. high']}</div>
+                <div> Low: {props.stock['04. low']}</div>
             </div>
 
             <div className="stock-card">
 
-                <div> {props.stock['01. symbol']}</div>
+                <div className="card-flex"> {props.stock['01. symbol']}</div>
                 <div className="card-flex">
                     <div> Price: {props.stock['05. price']}</div>
-                    <div> {props.stock['09. change']}</div>
-                    <div> {props.stock['10. change percent']}</div>
+                    <div> Change:{props.stock['09. change']}</div>
+                    <div> Percent Change:{props.stock['10. change percent']}</div>
                 </div>
                 <div className="card-flex">
                     <div> Open: {props.stock['02. open']}</div>
